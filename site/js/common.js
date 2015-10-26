c7571576e77c21f5153f796808d5915f65a46099
zheselevko <zhe.selevko@ya.ru>
2015-10-26 15:53:23 +0200

    end styling form

--- site/js/common.js
+++ site/js/common.js
@@ -1,19 +1,9 @@
 $(document).ready(function() {
-	
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
-
-	console.log($('body').html());
+    $('select').each(function(){
+        $(this).siblings('p').text( $(this).children('option:selected').text() );
+    });
+    $('select').change(function(){
+        $(this).siblings('p').text( $(this).children('option:selected').text() );
+    });
 });
